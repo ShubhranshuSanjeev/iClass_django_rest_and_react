48273fdc9699c81ae5697e3cb0a276134f1508db
Shubhranshu Sanjeev <shubhranshu.lumia@gmail.com>
2020-05-09 18:39:01 +0530

    Updated Models

--- accounts/models.py
+++ accounts/models.py
@@ -4,76 +4,59 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
-    use_in_migrations = True
+  use_in_migrations = True
 
-    def _create_user(self, email, password, username, is_student, is_teacher, **extra_fields):
-        if not email:
-            raise ValueError(_("The email value must be set"))
-        if not username:
-            raise ValueError(_("Username must be provided"))
-        if not password:
-            raise ValueError(_("Password must be set"))
+  def _create_user(self, email, password, username, is_student, is_teacher, **extra_fields):
+    if not email:
+        raise ValueError(_("The email value must be set"))
+    if not username:
+        raise ValueError(_("Username must be provided"))
+    if not password:
+        raise ValueError(_("Password must be set"))
 
-        email   = self.mormalize_email(email)
-        user    = self.model(email=email, username=username, is_student=is_student, is_teacher=is_teacher, **extra_fields)
-        user.set_password(password)
-        user.save(using=self.db)
+    email   = self.mormalize_email(email)
+    user    = self.model(email=email, username=username, is_student=is_student, is_teacher=is_teacher, **extra_fields)
+    user.set_password(password)
+    user.save(using=self.db)
 
-        return user
+    return user
 
-    def create_user(self, email, password=None, username=None, is_student=False, is_teacher=False, **extra_fields):
-        extra_fields.setdefault('is_superuser', False)
-        return _create_user(email, password, username, is_student, is_teacher, **extra_fields)
-    
-    def create_superuser(self, email, password=None, username=None, is_student=False, is_teacher=False, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_superuser') is False:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self._create_user(email, password, username, is_student, is_teacher, **extra_fields)
+  def create_user(self, email, password=None, username=None, is_student=False, is_teacher=False, **extra_fields):
+    extra_fields.setdefault('is_superuser', False)
+    return _create_user(email, password, username, is_student, is_teacher, **extra_fields)
+  
+  def create_superuser(self, email, password=None, username=None, is_student=False, is_teacher=False, **extra_fields):
+    extra_fields.setdefault('is_superuser', True)
+    if extra_fields.get('is_superuser') is False:
+        raise ValueError(_('Superuser must have is_superuser=True.'))
+    return self._create_user(email, password, username, is_student, is_teacher, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username            = models.CharField(_("username") ,max_length = 30, primary_key=True)
-    email               = models.EmailField(_("email"),max_length=60, unique=True)
-    first_name          = models.CharField(_("first name"), max_length=30, blank=True)
-    last_name           = models.CharField(_("last name"), max_length=30, blank=True)
-    is_student          = models.BooleanField(_("student"), default=False)
-    is_teacher          = models.BooleanField(_("teacher"), default=False)
-    avatar              = models.ImageField(upload_to='avatars/', null=True, blank=True)
-    
-    date_joined         = models.DateTimeField(verbose_name="Date Joined", auto_now_add=True)
-    last_login          = models.DateTimeField(verbose_name="Last Login", auto_now=True)
-    is_active           = models.BooleanField(verbose_name="Active", default=True)
+  username            = models.CharField(_("username"), max_length=30, unique=True, blank=False, null=False)
+  email               = models.EmailField(_("email"),max_length=60, unique=True, blank=False, null=False)
+  first_name          = models.CharField(_("first name"), max_length=30, blank=True)
+  last_name           = models.CharField(_("last name"), max_length=30, blank=True)
+  is_student          = models.BooleanField(_("student"), default=False)
+  is_teacher          = models.BooleanField(_("teacher"), default=False)
+  avatar              = models.ImageField(upload_to='avatars/', null=True, blank=True)
+  
+  date_joined         = models.DateTimeField(verbose_name="Date Joined", auto_now_add=True)
+  last_login          = models.DateTimeField(verbose_name="Last Login", auto_now=True)
+  is_active           = models.BooleanField(verbose_name="Active", default=True)
 
-    objects = CustomUserManager()
+  objects = CustomUserManager()
 
-    USERNAME_FIELD      = "email"
-    REQUIRED_FIELDS     = []
+  USERNAME_FIELD      = "email"
+  REQUIRED_FIELDS     = []
 
-    class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
-    
-    def get_full_name(self):
-        full_name = ' '.join([self.first_name, self.last_name])
-        return full_name
+  class Meta:
+    verbose_name = _("user")
+    verbose_name_plural = _("users")
 
-    def get_short_name(self):
-        return self.first_name
-
-    def get_classrooms(self):
-        if self.is_teacher:
-            return self.taking_classes.all()
-        else:
-            return self.attending_classes.get_related('room_number','course_name', 'teacher_id').all()
-
-class Teacher(models.Model):
-    user = models.OneToOneField(User, related_name="teacher", on_delete=models.CASCADE)
-
-    def get_user(self):
-        return self.user
-
-class Student(models.Model):
-    user = models.OneToOneField(User, related_name="student", on_delete=models.CASCADE)
-
-    def get_user(self):
-        return self.user
+  def __str__(self):
+    return self.username
+  
+  def get_classrooms(self):
+    if self.is_student:
+      return self.joined_classrooms.all()
+    return self.teaching_classrooms.all()
--- classroom/models.py
+++ classroom/models.py
@@ -1,57 +1,90 @@
 from django.db import models
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-from accounts.models import Student, Teacher
+
+def upload_assignment_file(instance, filename):
+	return "assignments/{classroom}/{filename}".format(
+            classroom=str(instance.classroom_id.classroom_id),
+            filename="Assignment"+str(instance.id)
+        )
+def upload_notes_file(instance, filename):
+	return "notes/{classroom}/{filename}".format(
+            classroom=str(instance.classroom_id.classroom_id),
+            filename="ReferenceMaterial"+str(instance.id)
+        )
+def upload_submission_file(instance, filename):
+    return "submissions/{classroom}/{filename}".format(
+        classroom=str(instance.classroom_id.classroom_id),
+        filename="Submission"+str(instance.id)
+    )
 
 class Classroom(models.Model):
-    classroom_id                    = models.UUIDField(_("classroom id"),primary_key=True, editable=False)
-    room_number                     = models.IntegerField(_("room number"),blank=True)
-    course_name                     = models.CharField(_("course name"), max_length=60, blank=False)
-    teacher_id                      = models.ForeignKey(Teacher, related_name="taking_classes", on_delete=models.CASCADE)
-    student_id                      = models.ManyToManyField(Student, related_name="attending_classes")
-    joining_permission              = models.BooleanField(_("joining permission"), default=True)
+  id                    = models.UUIDField(_("classroom id"), primary_key=True, editable=False)
+  room_number           = models.IntegerField(_("room number"),blank=True)
+  course_name           = models.CharField(_("course name"), max_length=60, blank=False)
+  teacher_id            = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="teaching_classrooms", on_delete=models.CASCADE)
+  joining_permission    = models.BooleanField(_("joining permission"), default=True)
 
-    class Meta:
-        verbose_name = _("Classroom")
-        verbose_name_plural = _("Classrooms")
+  class Meta:
+    verbose_name = _("Classroom")
+    verbose_name_plural = _("Classrooms")
 
-    def __str__(self):
-        return self.course_name
-    
-    def get_teacher(self):
-        return self.teacher_id
-    
-    def get_sudents(self):
-        return self.student_id.all()
-    
-    def get_assignments(self):
-        return self.assignments.all()
-    
-    def get_notes(self):
-        return self.notes.all()
-    
-    def get_assignment_submissions(self):
-        return self.get_assignment_submissions.all()
+  def __str__(self):
+    return self.course_name
+  
+  def get_teacher(self):
+    return self.teacher
 
-class JoinQueue(models.Model):
-    classroom_id            = models.ForeignKey(Classroom, related_name='awaiting_join_requests', on_delete=models.CASCADE)
-    student_id              = models.ForeignKey(Student, related_name='join_request_queued', on_delete=models.CASCADE)
+  def get_sudents(self):
+    return self.classroom_students.all()
+  
+  def get_assignments(self):
+    return self.assignments.all()
+  
+  def get_notes(self):
+    return self.reference_materials.all()
+  
+  def get_assignment_submissions(self):
+    return self.assignment_submissions.all()
+  
+  def get_pending_join_requests(self):
+    return self.pending_requests.all()
+
+class ClassroomStudents(models.Model):
+  classroom_id  = models.ForeignKey(Classroom, related_name="classroom_students", on_delete=models.CASCADE)
+  student_id    = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="joined_classrooms", on_delete=models.CASCADE)
+
+  class Meta:
+    unique_together = (('classroom_id', 'student_id'), )
+
+class JoinRequests(models.Model):
+  classroom_id  = models.ForeignKey(Classroom, related_name='pending_requests', on_delete=models.CASCADE)
+  student_id    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+  class Meta:
+    unique_together = (('classroom_id', 'student_id'), )
 
 class Assignment(models.Model):
-    classroom_id            = models.ForeignKey(Classroom, related_name="assignments", on_delete=models.CASCADE)
-    description             = models.CharField(_("description"), max_length=75, blank=False)
-    question                = models.FileField(upload_to='assignment_questions/', blank=False)
-    max_marks               = models.IntegerField(default=100)
+  classroom_id  = models.ForeignKey(Classroom, related_name="assignments", on_delete=models.CASCADE)
+  teacher       = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="owns_assignment", on_delete=models.CASCADE)
+  description   = models.CharField(_("description"), max_length=75, blank=False)
+  file          = models.FileField(upload_to=upload_assignment_file, blank=False)
+  max_marks     = models.IntegerField(default=100)
 
-    def get_submissions(self):
-        return self.assignment_submissions.all()
+  def get_submissions(self):
+    return self.assignment_submissions.all()
 
-class Note(models.Model):
-    classroom_id            = models.ForeignKey(Classroom, related_name="notes", on_delete=models.CASCADE)
-    description             = models.CharField(max_length=100, blank=False)
-    file                    = models.FileField(upload_to="notes/", blank=False)
+class ReferenceMaterial(models.Model):
+  classroom_id    = models.ForeignKey(Classroom, related_name="reference_materials", on_delete=models.CASCADE)
+  teacher_id      = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="owns_reference_material", on_delete=models.CASCADE)
+  description     = models.CharField(max_length=100, blank=False)
+  file            = models.FileField(upload_to=upload_notes_file, blank=False)
 
 class AssignmentSubmission(models.Model):
-    assignment_id           = models.ForeignKey(Assignment, related_name="assignment_submissions", on_delete=models.CASCADE)
-    student_id              = models.ForeignKey(Student, related_name="submitted_solutions", on_delete=models.CASCADE)
-    solution_file           = models.FileField(upload_to="assignment_submission", blank=False)
-    marks                   = models.PositiveIntegerField()
+  assignment_id   = models.ForeignKey(Assignment, related_name="assignment_submissions", on_delete=models.CASCADE)
+  student_id      = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="owns_solution", on_delete=models.CASCADE)
+  file            = models.FileField(upload_to=upload_submission_file, blank=False)
+  marks           = models.PositiveIntegerField()
+
+  class Meta:
+    unique_together = (('assignment_id', 'student_id'),)
